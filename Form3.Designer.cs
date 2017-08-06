29691c8c700f7ad78bc3f2719ca68c74bfb2ac75
coolpulkit99 <coolpulkit99@gmail.com>
2017-08-07 01:49:21 +0530

    Add files via upload

---
+++ Form3.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace project_final
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.database1DataSet = new project_final.Database1DataSet();
+            this.sheet3BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.sheet3TableAdapter = new project_final.Database1DataSetTableAdapters.Sheet3TableAdapter();
+            this.tableAdapterManager = new project_final.Database1DataSetTableAdapters.TableAdapterManager();
+            this.sheet3DataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fillByToolStrip = new System.Windows.Forms.ToolStrip();
+            this.dataToolStripLabel = new System.Windows.Forms.ToolStripLabel();
+            this.dataToolStripTextBox = new System.Windows.Forms.ToolStripTextBox();
+            this.fillByToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sheet3BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sheet3DataGridView)).BeginInit();
+            this.fillByToolStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // sheet3BindingSource
+            // 
+            this.sheet3BindingSource.DataMember = "Sheet3";
+            this.sheet3BindingSource.DataSource = this.database1DataSet;
+            // 
+            // sheet3TableAdapter
+            // 
+            this.sheet3TableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.Sheet1TableAdapter = null;
+            this.tableAdapterManager.Sheet3TableAdapter = this.sheet3TableAdapter;
+            this.tableAdapterManager.UpdateOrder = project_final.Database1DataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // sheet3DataGridView
+            // 
+            this.sheet3DataGridView.AllowUserToAddRows = false;
+            this.sheet3DataGridView.AllowUserToDeleteRows = false;
+            this.sheet3DataGridView.AutoGenerateColumns = false;
+            this.sheet3DataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.sheet3DataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.sheet3DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.sheet3DataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1});
+            this.sheet3DataGridView.DataSource = this.sheet3BindingSource;
+            this.sheet3DataGridView.Location = new System.Drawing.Point(12, 32);
+            this.sheet3DataGridView.Name = "sheet3DataGridView";
+            this.sheet3DataGridView.ReadOnly = true;
+            this.sheet3DataGridView.RowTemplate.Height = 24;
+            this.sheet3DataGridView.Size = new System.Drawing.Size(756, 391);
+            this.sheet3DataGridView.TabIndex = 1;
+            this.sheet3DataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.sheet3DataGridView_CellContentClick);
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Data";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Data";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            this.dataGridViewTextBoxColumn1.Width = 67;
+            // 
+            // fillByToolStrip
+            // 
+            this.fillByToolStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.fillByToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dataToolStripLabel,
+            this.dataToolStripTextBox,
+            this.fillByToolStripButton,
+            this.toolStripButton1});
+            this.fillByToolStrip.Location = new System.Drawing.Point(0, 0);
+            this.fillByToolStrip.Name = "fillByToolStrip";
+            this.fillByToolStrip.Size = new System.Drawing.Size(1115, 27);
+            this.fillByToolStrip.TabIndex = 2;
+            this.fillByToolStrip.Text = "fillByToolStrip";
+            // 
+            // dataToolStripLabel
+            // 
+            this.dataToolStripLabel.Name = "dataToolStripLabel";
+            this.dataToolStripLabel.Size = new System.Drawing.Size(44, 24);
+            this.dataToolStripLabel.Text = "Data:";
+            // 
+            // dataToolStripTextBox
+            // 
+            this.dataToolStripTextBox.Name = "dataToolStripTextBox";
+            this.dataToolStripTextBox.Size = new System.Drawing.Size(100, 27);
+            // 
+            // fillByToolStripButton
+            // 
+            this.fillByToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.fillByToolStripButton.Name = "fillByToolStripButton";
+            this.fillByToolStripButton.Size = new System.Drawing.Size(57, 24);
+            this.fillByToolStripButton.Text = "Search";
+            this.fillByToolStripButton.Click += new System.EventHandler(this.fillByToolStripButton_Click);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(44, 24);
+            this.toolStripButton1.Text = "Back";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(796, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(307, 391);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(284, 437);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(263, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Click the point in the table to view image.";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1115, 472);
+            this.ControlBox = false;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.fillByToolStrip);
+            this.Controls.Add(this.sheet3DataGridView);
+            this.MaximumSize = new System.Drawing.Size(1133, 519);
+            this.MinimumSize = new System.Drawing.Size(1133, 519);
+            this.Name = "Form3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Search  by Points";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sheet3BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sheet3DataGridView)).EndInit();
+            this.fillByToolStrip.ResumeLayout(false);
+            this.fillByToolStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Database1DataSet database1DataSet;
+        private System.Windows.Forms.BindingSource sheet3BindingSource;
+        private Database1DataSetTableAdapters.Sheet3TableAdapter sheet3TableAdapter;
+        private Database1DataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.DataGridView sheet3DataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.ToolStrip fillByToolStrip;
+        private System.Windows.Forms.ToolStripLabel dataToolStripLabel;
+        private System.Windows.Forms.ToolStripTextBox dataToolStripTextBox;
+        private System.Windows.Forms.ToolStripButton fillByToolStripButton;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Form3.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project_final
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void sheet3BindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.sheet3BindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.database1DataSet);
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'database1DataSet.Sheet3' table. You can move, or remove it, as needed.
+            this.sheet3TableAdapter.Fill(this.database1DataSet.Sheet3);
+
+        }
+
+        private void fillByToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.sheet3TableAdapter.FillBy(this.database1DataSet.Sheet3, "%"+ dataToolStripTextBox.Text+ "%");
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            Form1 frm = new Form1();
+            frm.Show(); this.Close();
+        }
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void sheet3DataGridView_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+
+        private void sheet3DataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {try {
+                int rowindex = e.RowIndex;
+                DataGridViewRow row = sheet3DataGridView.Rows[rowindex];
+                String s = row.Cells[0].Value + "";
+                String s2 = s.Split('-')[0];
+             //MessageBox.Show(s2.Trim());
+                string path = ("Resources/" + s2.Trim() + ".jpg").Replace(" ", "");
+              //    MessageBox.Show(path);
+                Image imgcircle = Image.FromFile(path);
+                pictureBox1.Image = imgcircle;
+                //   MessageBox.Show(s2);
+            }
+
+            catch (Exception) { }
+        }
+    }
+}
